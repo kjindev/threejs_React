b167f5ca1943c976699abd88a1990cd3b4ecf4e7
kjin <pkjin.eng@gmail.com>
2023-02-01 21:31:15 +0900

    cube rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./index.css";
+
+function Box({ time, ...props }) {
+  return (
+    <mesh {...props}>
+      <boxGeometry />
+      <meshStandardMaterial roughness={0.75} />
+    </mesh>
+  );
+}
+
+function Content() {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.015));
+  return (
+    <group ref={ref}>
+      <Box position={[0, 0, 0]} />
+    </group>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 10, 0] }}>
+      <pointLight color="black" />
+      <pointLight position={[10, 10, -10]} color="orange" />
+      <pointLight position={[-10, -10, 10]} color="white" />
+      <Content />
+    </Canvas>
   );
 }
 
